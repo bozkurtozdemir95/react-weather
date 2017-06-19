8484ce27164d7efc2117a9541f102fc4899a4015
bozkurtozdemir95 <bozkurtozdemir95@gmail.com>
2017-06-19 15:35:02 +0400

    country changes

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -7,8 +7,9 @@ export default class Header extends React.Component{
     render(){
         return(
             <div className="header">
-                <h1>{this.props.daily.name}</h1>
-                {this.props.daily.sys ? <span>{this.props.daily.sys.country}</span> : ''}
+                <h1 className="country">{this.props.daily.name} 
+                    {this.props.daily.sys ? <span>{this.props.daily.sys.country}</span> : ''}
+                </h1>
                 <input type="text" placeholder="Şehir adı giriniz.." value={this.props.cityId.value} onChange={this.props.handleChange.bind(this)}/>
             </div>
         )
